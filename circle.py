be2244108d013e715e528df676d7b1d49930b74a
jayashreepu <88489428+jayashreepu@users.noreply.github.com>
2021-08-05 16:59:29 +0530

    Delete circle.py

--- circle.py
+++
@@ -1,8 +0,0 @@
-Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> 
->>> 
- RESTART: C:/Users/Karthik P/AppData/Local/Programs/Python/Python37-32/circle.py 
-Input the radius of the circle : 1.1
-The area of the circle with radius 1.1 is 3.8013271108436504
->>> 
